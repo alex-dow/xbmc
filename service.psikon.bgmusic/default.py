3070eb565f51e42de27c0cd29693ad723f01717a
v0idnull <v0idnull@gmail.com>
2013-08-12 13:35:22 -0400

    Configuration
    Made the background music selection configurable.

--- service.psikon.bgmusic/default.py
+++ service.psikon.bgmusic/default.py
@@ -1,22 +1,30 @@
 import xbmc
 import time
 
+RUN = True
+
+SELF = xbmc.Addon('service.psikon.bgmusic')
+
 def should_play():
+	media = SELF.getSetting('psikon.media')
+	if !media:
+		return false
 	return not xbmc.getCondVisibility("Player.Playing")
 
 
 def playmusic(media):
+
+	
 	xbmc.executebuiltin("PlayMedia(%s)" % media)
 	xbmc.executebuiltin("PlayerControl(RandomOn)")
 	bg_active = True
 
 	
-RUN = True
 
 	
 while RUN: 
 	if (should_play()):
-		playmusic("special://home/addons/service.psikon.bgmusic/resources/music.mp3")
+		playmusic(SELF.getSetting('psikon.media'))
 		
 	xbmc.sleep(2000)
 
